34a3a50344ac96707606b15ba806f8236958e60b
ellykouneli <carasic88@gmail.com>
2025-10-31 20:22:49 +0200

    Add eslint ignoreDuringBuilds for Vercel

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Skip ESLint checks during Vercel build
+  },
 };
 
 export default nextConfig;
